154b19611ae3df687d8ecae417dacbf43cad29e9
gkgitHubcom <102565079+gkgitHubcom@users.noreply.github.com>
2022-04-09 21:41:04 +0800

    Delete day02 directory

--- day02/1-知识拓展/2-zip用法.py
+++
@@ -1,5 +0,0 @@
-a = ["class","teacher","student"]
-b = ["py37","zhangsan","wangwu"]
-
-res = dict(zip(a,b))
-print(res)
--- day02/1-知识拓展/5-faker造数据.py
+++
@@ -1,4 +0,0 @@
-from faker import Faker
-
-f = Faker("zh_CN")
-print(f.phone_number())
